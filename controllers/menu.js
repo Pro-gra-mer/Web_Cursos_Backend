50ab486a8662bd7766119b6061a1818c944dc9e7
Rebeca <rbk_po@hotmail.com>
2024-04-21 17:05:35 +0200

    Sistema de menús

---
+++ controllers/menu.js
@@ -0,0 +1,55 @@
+const Menu = require("../models/menu");
+
+async function createMenu(req, res) {
+  try {
+    const menu = new Menu(req.body);
+    const menuStored = await menu.save();
+    return res.status(200).send(menuStored);
+  } catch (error) {
+    return res.status(400).send({ msg: "Error al crear el menu" });
+  }
+}
+
+async function getMenus(req, res) {
+  const { active } = req.query;
+
+  let response = null;
+  if (active === undefined) {
+    response = await Menu.find().sort({ order: "asc" });
+  } else {
+    response = await Menu.find({ active }).sort({ order: "asc" });
+  }
+  if (!response) {
+    return res.status(400).send({ msg: "No se ha encontrado ningún menú" });
+  } else {
+    return res.status(200).send(response);
+  }
+}
+
+async function updateMenu(req, res) {
+  try {
+    const { id } = req.params;
+    const menuData = req.body;
+    await Menu.findByIdAndUpdate(id, menuData);
+    return res.status(200).send({ msg: "Actualización correcta" });
+  } catch (error) {
+    return res.status(400).send({ msg: "Error al actualizar el menú" });
+  }
+}
+
+async function deleteMenu(req, res) {
+  try {
+    const { id } = req.params;
+    await Menu.findByIdAndDelete(id);
+    return res.status(200).send({ msg: "Menú eliminado" });
+  } catch (error) {
+    return res.status(400).send({ msg: "Error al eliminar el menú" });
+  }
+}
+
+module.exports = {
+  createMenu,
+  getMenus,
+  updateMenu,
+  deleteMenu,
+};
